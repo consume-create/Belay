1f4694d791dd02849aaad7232e277201e71509c1
sam-consume <88338524+sam-consume@users.noreply.github.com>
2021-08-18 11:41:26 -0600

    added social icons and links to footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,18 +1,59 @@
 <template>
+  <section class="footer">
+    <div class="form-cta">
 
-  <h3>Hey its the footer</h3>
+
+    </div>
+
+    <div class="channels">
+      <h5 class="small-title"> Our Channels</h5>
+      <p class="email">info@belaydev.com</p>
+      <p class="phone-number"> 303 / 607 /6133</p>
+
+      <SocialLinks :links="
+      [
+        {
+          'type': 'twitter',
+          'url': 'https://twitter.com/facewaretech'
+        },
+        {
+          'type': 'linkedin',
+          'url': 'https://www.linkedin.com/company/faceware-technologies-inc-'
+        },
+        {
+          'type': 'discord',
+          'url': 'https://discord.gg/ZurczeCeRY'
+        }
+      ]" />
+
+    </div>
 
 
 
+  </section>
 </template>
 
 
 <script>
-  export default{}
+import SocialLinks from "~/components/social-links.vue";
+export default{
+  components: {
+    SocialLinks 
+  }
+
+}
 
 </script>
 
 <style lang="scss">
+
+.channels{
+  padding: span(6,28);
+  background-color: $light-greg;
+
+
+}
+
 
 
 
---
+++ components/social-links.vue
@@ -0,0 +1,74 @@
+<template>
+  <ul class="socials">
+    <li v-for="link in links" :key="link.url">
+      <a :class="link.type" :href="link.url" target="_blank" rel="noreferrer noopener" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Array
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/module_base.scss';
+
+$iconSize: 22px;
+
+ul.socials {
+  width: 100%;
+  margin: -9px 0 $margin-normal -9px;
+  display: flex;
+  li {
+    display: flex;
+    a {
+      position: relative;
+      width: 40px;
+      height: 40px;
+      &:after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: $iconSize;
+        height: $iconSize;
+        margin: -11px 0 0 -11px;
+      }
+      &.twitter:after {
+        @include icon("instagram", $deep-blue, true);
+      }
+      &.linkedin:after {
+        @include icon("linkedin", $deep-blue, true);
+      }
+      &.discord:after {
+        @include icon("twitter",$deep-blue, true);
+        // margin-top: -10px;
+      }
+    }
+  }
+  @include can-hover {
+    li {
+      a {
+        &:after {
+          transition: background-image $speed-demon $ease-out;
+        }
+        &.twitter:hover:after {
+          @include icon("instagram",$white, false);
+        }
+        &.linkedin:hover:after {
+          @include icon("linkedin", $white, false);
+        }
+        &.discord:hover:after {
+          @include icon("twitter",$white, false);
+        }
+      }
+    }
+  }
+}
+</style>
