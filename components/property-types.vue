2919d67f84d6313141926f230de1572dd90a85cd
sam-consume <88338524+sam-consume@users.noreply.github.com>
2021-08-23 11:57:35 -0600

    Grid right for static size

--- components/property-types.vue
+++ components/property-types.vue
@@ -8,10 +8,11 @@
         <ResponsiveImage :src="`${img1.src}`" :alt="`${img1.alt}`" lazy />
     </div>
 
+    <div class="canvas-wrapper">
       <div class="canvas-outer">
-         <canvas @mousemove="breakDance" id="cnv" width="1000" height="600"></canvas>
-          <!-- <canvas id="cnv"></canvas> -->
+         <canvas @mousemove="breakDance" id="cnv" width="1900" height="1400"></canvas>
       </div>
+    </div>
   </section>
 </template>
 
@@ -22,7 +23,7 @@ import gsap from "gsap";
 import EasePack from "~/plugins/EasePack";
 export default{
   components: {
-    ResponsiveImage
+    ResponsiveImage,
   },
   props: {
     img1:
@@ -57,17 +58,17 @@ export default{
   mounted() {
     var _cnv = document.getElementById("cnv");
     var _ctx = _cnv.getContext("2d");
+    this._cnv = _cnv;
     this._ctx = _ctx;
-    this._cnv = _ctx;
-    this._width= 1000;
-    this._height= 600;
+    this._width= 1700;
+    this._height= 1000;
     this._rows = 3;
     this._cols = 5;
     this._points = [];
     this._old_row = null;
     this._old_col = null;
     this.init();
-    this.loop();
+    window.requestAnimationFrame(this.loop);
     // this.renderLines();
     // window.requestAnimationFrame(this.loop);
 
@@ -76,8 +77,9 @@ export default{
     init() {
       for(let i = 0; i <= this._rows; i++) {
         for(let j = 0; j <= this._cols; j++) {
-          let x = (50 + (j * (this._width / this._cols))),
-              y = (50 + (i * (this._height / this._rows)));
+          let x = (50+ (j * (this._width / this._cols))),
+              y = (50+ (i * (this._height / this._rows)));
+              console.log(x);
 
           this._points.push({x, y, clean_x: x, clean_y: y});
         }
@@ -87,7 +89,7 @@ export default{
     },
 
     renderLines() {
-      this._ctx.clearRect(0, 0, this._width, this._height);
+      this._ctx.clearRect(0, 0, this._width+100, this._height+100);
       this._ctx.beginPath();
       this._ctx.moveTo(this._points[0].x, this._points[0].y);
 
@@ -112,14 +114,15 @@ export default{
     },
 
     breakDance (event) {
-      let mx = ((event.offsetX * 2) - 50);
-      let my = ((event.offsetY * 2) - 50);
+      let mx = ((event.offsetX ) - 50);
+      let my = ((event.offsetY ) - 50);
+      console.log(mx,my);
 
       if(mx > 0 && my > 0 && mx < this._width && my < this._height) {
 
         let row = Math.floor(my / (this._height / this._rows));
         let col = Math.floor(mx / (this._width / this._cols));
-        console.log(col, row);
+
         if(row !== this._old_row || col !== this._old_col) {
           this.translate([
             (col + (row * (this._cols + 1))), // upper left
@@ -136,13 +139,13 @@ export default{
       else {
         this.translate([]);
         this._old_row = this._old_col = -1;
+        console.log("here");
       }
     },
 
     loop() {
       this.renderLines();
       window.requestAnimationFrame(this.loop);
-      // console.log("here");
     },
   },
 }
@@ -152,6 +155,7 @@ export default{
 <style lang="scss">
   .property-wrapper{
     margin-top: span(4);
+    position: relative;
 
     .title{
         text-align: center;
@@ -162,7 +166,8 @@ export default{
 
     .property-image {
       position: relative;
-      width: span(28);
+      width: span(24);
+      margin: 0 auto;
 
       img {
         position: absolute;
@@ -173,18 +178,27 @@ export default{
       }
     }
 
-    // .canvas-outer{
-    //   position: relative;
-    //   // width: span(28);
-    //
-    //   #cnv{
-    //     position: absolute;
-    //     width: 100%;
-    //     height: 100%;
-    //     top: 0px;
-    //     left: 0px;
-    //   }
-    // }
+  .canvas-wrapper{
+    position: absolute;
+    top: 1%;
+    left: 0%;
+
+    .canvas-outer{
+      position: relative;
+      width: span(24);
+      z-index: 12;
+      margin: 0 auto;
+
+      #cnv {
+        position: absolute;
+        top: 0%;
+        left: 0%;
+        // width: 100%;
+        // height: 100%;
+        // transform: translate(-50%, -50%);
+      }
+    }
+  }
   }
 
 
